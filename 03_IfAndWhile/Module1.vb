1eff3e2318d1ecc2a385282980e3bb219f985b12
Lee Macri <lem@rathkeale.school.nz>
2019-06-13 10:36:04 +1200

    Lesson 3 Add

---
+++ 03_IfAndWhile/Module1.vb
@@ -0,0 +1,25 @@
+﻿''' <summary>
+'''     This exercise will introduce you to if statements and while loops.
+''' </summary>
+''' <author> Mr Macri </author>
+''' <date> 13Jun19 </date>
+''' 
+Module Module1
+
+    Sub Main()
+        ' Housekeeping
+        Dim UserResponse As String
+
+        ' Ask user if they like coffee and record their answer.
+        Console.WriteLine("Do you like coffee?")
+        UserResponse = Console.ReadLine()
+
+        ' Check input and respond
+
+
+        ' Pause program to let the user read 
+        Console.ReadLine()
+
+    End Sub
+
+End Module
