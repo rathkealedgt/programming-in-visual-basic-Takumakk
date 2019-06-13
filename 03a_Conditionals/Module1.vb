b4dee792643efe4f729250120783799e4b3ca0ef
Lee Macri <lem@rathkeale.school.nz>
2019-06-13 12:51:18 +1200

    Lesson 3 Added

---
+++ 03a_Conditionals/Module1.vb
@@ -0,0 +1,27 @@
+﻿''' <summary>
+'''     This program sasks the user for their age. Then the program will show if the person is eligible to vote. 
+'''     A person who is eligible to vote must be older than or equal to 18 years old.
+''' </summary>
+''' <author> </author>
+''' <date> </date>
+Module Module1
+
+    Sub Main()
+        'Housekeeping
+
+
+        ' Ask the user to enter their age and store it
+
+
+        ' If the user is 18 or over, tell them they can vote.
+
+
+        ' Oherwise, tell them they cannot vote.
+
+
+        ' Print a message to press q to quit
+
+
+    End Sub
+
+End Module
