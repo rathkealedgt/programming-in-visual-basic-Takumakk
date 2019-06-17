4fef8d0e9bad9963d540bb625a5327ea62b04bf5
Lee Macri <lem@rathkeale.school.nz>
2019-06-17 17:02:40 +1200

    Add Lesson 04
    Try Catch blocks

---
+++ 04_TryCatch/Module1.vb
@@ -0,0 +1,21 @@
+﻿''' <summary>
+'''     This program asks the user for a number until they enter a number between 1 and 10.
+''' </summary>
+''' <author> Mr M </author>
+''' <date> 13Jun19 </date>
+Module Module1
+
+    Sub Main()
+        'Housekeeping
+        Dim UserResponse As Integer
+
+        ' Ask user for a number and store it.
+        Console.WriteLine("Please enter an integer between 1 and 10.")
+        UserResponse = Console.ReadLine()
+
+        ' Pause the program to view output.
+        Console.ReadLine()
+
+    End Sub
+
+End Module
